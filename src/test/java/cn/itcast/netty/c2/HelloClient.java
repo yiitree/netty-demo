16ad9148a055ecfb046b91a3cca6d3189f12fdaa
Jerry.Zeng <Jerry.Zeng@cdpgroupltd.com>
2021-11-22 13:53:30 +0800

    netty

--- src/test/java/cn/itcast/netty/c2/HelloClient.java
+++ src/test/java/cn/itcast/netty/c2/HelloClient.java
@@ -8,26 +8,35 @@ import io.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
 
+/**
+ * 客户端
+ */
 public class HelloClient {
+
     public static void main(String[] args) throws InterruptedException {
         // 1. 启动类
         new Bootstrap()
-            // 2. 添加 EventLoop
-            .group(new NioEventLoopGroup())
-            // 3. 选择客户端 channel 实现
-            .channel(NioSocketChannel.class)
-            // 4. 添加处理器
-            .handler(new ChannelInitializer<NioSocketChannel>() {
-                @Override // 在连接建立后被调用
-                protected void initChannel(NioSocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new StringEncoder());
-                }
-            })
-            // 5. 连接到服务器
-            .connect(new InetSocketAddress("localhost", 8080))
-            .sync()
-            .channel()
-            // 6. 向服务器发送数据
-            .writeAndFlush("hello, world");
+                // 2. 添加 EventLoop
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(
+                        new ChannelInitializer<NioSocketChannel>() {
+                            // 在连接建立后被调用
+                            @Override
+                            protected void initChannel(NioSocketChannel ch) throws Exception {
+                                // 对发送的内容进行编码，把字符串转化为byteBuffer
+                                ch.pipeline().addLast(new StringEncoder());
+                            }
+                        })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+
+                .sync()
+                .channel()
+                // 6. 向服务器发送数据
+                .writeAndFlush("hello, world");
     }
+
 }
--- src/test/java/cn/itcast/netty/c2/HelloServer.java
+++ src/test/java/cn/itcast/netty/c2/HelloServer.java
@@ -10,32 +10,53 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.logging.LoggingHandler;
 
+/**
+ * 服务端
+ */
 public class HelloServer {
+
     public static void main(String[] args) {
-        // 1. 启动器，负责组装 netty 组件，启动服务器
+
+        // 1. 服务端的启动器，负责组装 netty 组件，启动服务器，类似springboot，直接启动，很方便。
         new ServerBootstrap()
-            // 2. BossEventLoop, WorkerEventLoop(selector,thread), group 组
-            .group(new NioEventLoopGroup())
-            // 3. 选择 服务器的 ServerSocketChannel 实现
-            .channel(NioServerSocketChannel.class) // OIO BIO
-            // 4. boss 负责处理连接 worker(child) 负责处理读写，决定了 worker(child) 能执行哪些操作（handler）
-            .childHandler(
-                    // 5. channel 代表和客户端进行数据读写的通道 Initializer 初始化，负责添加别的 handler
-                new ChannelInitializer<NioSocketChannel>() {
-                @Override
-                protected void initChannel(NioSocketChannel ch) throws Exception {
-                    // 6. 添加具体 handler
-                    ch.pipeline().addLast(new LoggingHandler());
-                    ch.pipeline().addLast(new StringDecoder()); // 将 ByteBuf 转换为字符串
-                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() { // 自定义 handler
-                        @Override // 读事件
-                        public void channelRead(ChannelHandlerContext ctx,Object msg) throws Exception {
-                            System.out.println(msg); // 打印上一步转换好的字符串
-                        }
-                    });
-                }
-            })
-            // 7. 绑定监听端口
-            .bind(8080);
+                // 添加组件
+                // 2. BossEventLoop, WorkerEventLoop(selector,thread), group 组
+                // NioEventLoopGroup -
+                // EventLoop：包含selector + 一个线程 - 一个线程负责一个事件
+                .group(new NioEventLoopGroup())
+                // 3. 选择 服务器的 ServerSocketChannel 实现
+                // NioServerSocketChannel其实就是netty对ServerSocketChannel的一个封装
+                // 还有OIO BIO
+                .channel(NioServerSocketChannel.class)
+                // 4. 处理事件的分工
+                // boss 负责处理连接
+                // worker(child) 负责处理读写，决定了 worker(child) 能执行哪些操作（handler）
+                // 其实就是指定worker执行什么逻辑
+                .childHandler(
+                        // 5. channel 代表和客户端进行数据读写的通道
+                        // Initializer 初始化，负责添加别的 handler
+                        new ChannelInitializer<NioSocketChannel>() {
+                            @Override
+                            protected void initChannel(NioSocketChannel ch) throws Exception {
+                                // 6. 添加具体 handler
+                                // LoggingHandler
+                                ch.pipeline().addLast(new LoggingHandler());
+                                // 解码handler：把接收的 ByteBuf 转换为字符串
+                                ch.pipeline().addLast(new StringDecoder());
+                                // 自定义 handler - 把读取的数据打印出来
+                                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                    // 读事件
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        // 打印上一步转换好的字符串
+                                        System.out.println(msg);
+                                    }
+                                });
+                            }
+                        })
+                // 7. 绑定监听端口
+                .bind(8080);
+
     }
+
 }
